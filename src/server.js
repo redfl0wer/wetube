0fb221efb2645d17c502072400f32d939889c1a5
redfl0wer <lion3032@naver.com>
2023-06-24 15:33:10 +0900

    request object and response object

--- src/server.js
+++ src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = (req, res) => {
+  return res.send("I Love You");
+};
+const handleLogin = (req, res) => {
+  return res.send("Login Here");
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () =>
   console.log(`👍 Server Listening on port https://localhost:${PORT}😊`);
 
