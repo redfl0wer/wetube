80b14ea8ea57be520798ef10f3fc35f682039fc9
redfl0wer <lion3032@naver.com>
2023-07-03 19:39:43 +0900

    practice some middlewares

--- src/server.js
+++ src/server.js
@@ -4,15 +4,23 @@ const PORT = 4000;
 
 const app = express();
 
+const loginMiddleware = (req, res, next) => {
+  console.log(`Someone is trying to go to ${req.url}`);
+  next();
+};
+const homeMiddleware = (req, res, next) => {
+  console.log(`Someone is trying to go to ${req.url}`);
+  next();
+};
 const handleHome = (req, res) => {
-  return res.send("I Love You");
+  return res.send({ message: "I Love You" });
 };
 const handleLogin = (req, res) => {
-  return res.send("Login Here");
+  return res.send("<h1>Login Here</h1>");
 };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/", homeMiddleware, handleHome);
+app.get("/login", loginMiddleware, handleLogin);
 
 const handleListening = () =>
   console.log(`👍 Server Listening on port https://localhost:${PORT}😊`);
