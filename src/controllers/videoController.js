ab57a1f9bbf77458136af61c4e7a95b802f7a301
redfl0wer <lion3032@naver.com>
2023-07-12 17:44:05 +0900

    inheritance and extend

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,6 @@
-export const trending = (req, res) => res.send("Trending Videos");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-};
-export const edit = (req, res) => {
-  console.log(req.params);
-  res.send("Edit");
-};
+export const trending = (req, res) => res.render("home");
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => {
   console.log(req.params);
