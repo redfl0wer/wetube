e5dca4e9520e9b245c1e1617fde7cf240bfd0671
redfl0wer <lion3032@naver.com>
2023-07-13 18:33:45 +0900

    iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,11 @@
-const fakeUser = {
-  username: "Woochan",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [
+    { title: "Video #1" },
+    { title: "Video #2" },
+    { title: "Video #3" },
+  ];
+  res.render("home", { pageTitle: "Home", videos: videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
