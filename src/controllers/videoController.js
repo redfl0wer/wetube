d643cba70963bb0ca96f7df9f79aa0166c43a66d
redfl0wer <lion3032@naver.com>
2023-07-25 15:06:49 +0900

    edit video, upload video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,37 +1,69 @@
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "Video #1",
-      rating: 5,
-      comments: 10,
-      uploadedAt: "2 days ago",
-      views: 51,
-      id: 1,
-    },
-    {
-      title: "Video #2",
-      rating: 5,
-      comments: 10,
-      uploadedAt: "2 days ago",
-      views: 51,
-      id: 2,
-    },
-    {
-      title: "Video #3",
-      rating: 5,
-      comments: 10,
-      uploadedAt: "2 days ago",
-      views: 51,
-      id: 3,
-    },
-  ];
+let videos = [
+  {
+    title: "Video #1",
+    rating: 5,
+    comments: 10,
+    uploadedAt: "2 days ago",
+    views: 51,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    rating: 5,
+    comments: 10,
+    uploadedAt: "2 days ago",
+    views: 51,
+    id: 2,
+  },
+  {
+    title: "Video #3",
+    rating: 5,
+    comments: 10,
+    uploadedAt: "2 days ago",
+    views: 1,
+    id: 3,
+  },
+];
+export const trending = (req, res) =>
   res.render("home", { pageTitle: "Home", videos: videos });
+export const watch = (req, res) => {
+  const id = req.params.id;
+  const video = videos[id - 1];
+  console.log(req.params);
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+export const getEdit = (req, res) => {
+  const id = req.params.id; //
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  const { id } = req.params; //
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
 export const search = (req, res) => res.send("Search");
 export const deleteVideo = (req, res) => {
   console.log(req.params);
   res.send("Delete Video");
 };
-export const upload = (req, res) => res.send("Upload Video");
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  // 비디오 array에 비디오 추가
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    uploadedAt: "Just Now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import { edit, deleteUser, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
-userRouter.get(":id", see);
+userRouter.get("/delete", deleteUser);
+userRouter.get("/:id", see);
 
 export default userRouter;
